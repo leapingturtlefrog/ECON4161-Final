a6d79869378e50a3062877691f0d620185220ce4
vvamp <99219780+vvamp86@users.noreply.github.com>
2025-04-29 16:02:52 -0400

    Update Visualization.R
    switch to total wealth

--- scripts/Visualization.R
+++ scripts/Visualization.R
@@ -5,11 +5,11 @@ library(ggplot2)
 # read data
 data <- read.csv("401k.csv")
 
-# form graph of distribution from PSet 1
-p401_density_plot <- ggplot(data, aes(x = net_tfa, fill = as.factor(p401))) +
+# altered graph of distribution from PSet 1, switching net total assets for total wealth
+p401_density_plot <- ggplot(data, aes(x = tw, fill = as.factor(p401))) +
   geom_density(alpha = 0.5) +
-  labs(title = "Distribution of Net Financial Assets by 401(k) Participation",
-       x = "Net Financial Assets", y = "Density", fill = "401(k) Participation") +
+  labs(title = "Distribution of Total Wealth by 401(k) Participation",
+       x = "Total Wealth", y = "Density", fill = "401(k) Participation") +
   theme_minimal()
 
 p401_density_plot
