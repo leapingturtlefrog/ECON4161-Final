53304e5d438b5164585c5c0359965a62c5762c1c
vvamp <99219780+vvamp86@users.noreply.github.com>
2025-04-29 15:59:32 -0400

    Update Visualization.R

--- scripts/Visualization.R
+++ scripts/Visualization.R
@@ -1,4 +1,12 @@
-p401_density_plot <- ggplot(table_data, aes(x = net_tfa, fill = as.factor(p401))) +
+# install.packages("ggplot2")
+# import packages
+library(ggplot2)
+
+# read data
+data <- read.csv("401k.csv")
+
+# form graph of distribution from PSet 1
+p401_density_plot <- ggplot(data, aes(x = net_tfa, fill = as.factor(p401))) +
   geom_density(alpha = 0.5) +
   labs(title = "Distribution of Net Financial Assets by 401(k) Participation",
        x = "Net Financial Assets", y = "Density", fill = "401(k) Participation") +
